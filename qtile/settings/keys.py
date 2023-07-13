ba6799cb17355e07476ff928013971266ddf0a35
Pablo Torres <pablo.torres.t@gmail.com>
2023-07-13 11:39:03 -0400

    use another for toggle sound to avoid losing the configuration

--- qtile/settings/keys.py
+++ qtile/settings/keys.py
@@ -10,85 +10,61 @@ from libqtile.command import lazy
 
 mod = "mod4"
 
-keys = [Key(key[0], key[1], *key[2:]) for key in [
-    # ------------ Window Configs ------------
-
-    # Switch between windows in current stack pane
-    ([mod], "j", lazy.layout.down()),
-    ([mod], "k", lazy.layout.up()),
-    ([mod], "h", lazy.layout.left()),
-    ([mod], "l", lazy.layout.right()),
-
-    # Change window sizes (MonadTall)
-    ([mod, "shift"], "l", lazy.layout.grow()),
-    ([mod, "shift"], "h", lazy.layout.shrink()),
-
-    # Toggle floating
-    ([mod, "shift"], "f", lazy.window.toggle_floating()),
-
-    # Move windows up or down in current stack
-    ([mod, "shift"], "j", lazy.layout.shuffle_down()),
-    ([mod, "shift"], "k", lazy.layout.shuffle_up()),
-
-    # Toggle between different layouts as defined below
-    ([mod], "Tab", lazy.next_layout()),
-    ([mod, "shift"], "Tab", lazy.prev_layout()),
-
-    # Kill window
-    ([mod], "w", lazy.window.kill()),
-
-    # Switch focus of monitors
-    ([mod], "period", lazy.next_screen()),
-    ([mod], "comma", lazy.prev_screen()),
-
-    # Restart Qtile
-    ([mod, "control"], "r", lazy.restart()),
-
-    ([mod, "control"], "q", lazy.shutdown()),
-    ([mod], "r", lazy.spawncmd()),
-
-    # ------------ App Configs ------------
-
-    # Menu
-    ([mod], "m", lazy.spawn("rofi -show drun")),
-
-    # Window Nav
-    ([mod, "shift"], "m", lazy.spawn("rofi -show")),
-
-    # Browser
-    ([mod], "b", lazy.spawn("google-chrome-stable")),
-
-    # File Explorer
-    ([mod], "e", lazy.spawn("alacritty -e ranger")),
-
-    # Processes
-    ([mod], "p", lazy.spawn("alacritty -e htop")),
-
-    # Terminal
-    ([mod], "Return", lazy.spawn("alacritty")),
-
-    # Redshift
-    ([mod], "r", lazy.spawn("redshift -O 2400")),
-    ([mod, "shift"], "r", lazy.spawn("redshift -x")),
-
-    # Screenshot
-    ([mod], "s", lazy.spawn("scrot")),
-    ([mod, "shift"], "s", lazy.spawn("scrot -s")),
-
-    # ------------ Hardware Configs ------------
-
-    # Volume
-    ([], "XF86AudioLowerVolume", lazy.spawn(
-        "amixer -q sset Master 3%-"
-    )),
-    ([], "XF86AudioRaiseVolume", lazy.spawn(
-        "amixer -q sset Master 3%+"
-    )),
-    ([], "XF86AudioMute", lazy.spawn(
-        "amixer -q sset Master toggle"
-    )),
-
-    # Brightness
-    ([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10%")),
-    ([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
-]]
+keys = [
+    Key(key[0], key[1], *key[2:])
+    for key in [
+        # ------------ Window Configs ------------
+        # Switch between windows in current stack pane
+        ([mod], "j", lazy.layout.down()),
+        ([mod], "k", lazy.layout.up()),
+        ([mod], "h", lazy.layout.left()),
+        ([mod], "l", lazy.layout.right()),
+        # Change window sizes (MonadTall)
+        ([mod, "shift"], "l", lazy.layout.grow()),
+        ([mod, "shift"], "h", lazy.layout.shrink()),
+        # Toggle floating
+        ([mod, "shift"], "f", lazy.window.toggle_floating()),
+        # Move windows up or down in current stack
+        ([mod, "shift"], "j", lazy.layout.shuffle_down()),
+        ([mod, "shift"], "k", lazy.layout.shuffle_up()),
+        # Toggle between different layouts as defined below
+        ([mod], "Tab", lazy.next_layout()),
+        ([mod, "shift"], "Tab", lazy.prev_layout()),
+        # Kill window
+        ([mod], "w", lazy.window.kill()),
+        # Switch focus of monitors
+        ([mod], "period", lazy.next_screen()),
+        ([mod], "comma", lazy.prev_screen()),
+        # Restart Qtile
+        ([mod, "control"], "r", lazy.restart()),
+        ([mod, "control"], "q", lazy.shutdown()),
+        ([mod], "r", lazy.spawncmd()),
+        # ------------ App Configs ------------
+        # Menu
+        ([mod], "m", lazy.spawn("rofi -show drun")),
+        # Window Nav
+        ([mod, "shift"], "m", lazy.spawn("rofi -show")),
+        # Browser
+        ([mod], "b", lazy.spawn("google-chrome-stable")),
+        # File Explorer
+        ([mod], "e", lazy.spawn("alacritty -e ranger")),
+        # Processes
+        ([mod], "p", lazy.spawn("alacritty -e htop")),
+        # Terminal
+        ([mod], "Return", lazy.spawn("alacritty")),
+        # Redshift
+        ([mod], "r", lazy.spawn("redshift -O 2400")),
+        ([mod, "shift"], "r", lazy.spawn("redshift -x")),
+        # Screenshot
+        ([mod], "s", lazy.spawn("scrot")),
+        ([mod, "shift"], "s", lazy.spawn("scrot -s")),
+        # ------------ Hardware Configs ------------
+        # Volume
+        ([], "XF86AudioLowerVolume", lazy.spawn("amixer -q sset Master 3%-")),
+        ([], "XF86AudioRaiseVolume", lazy.spawn("amixer -q sset Master 3%+")),
+        ([], "XF86AudioMute", lazy.spawn("pactl -- set-sink-mute 0 toggle")),
+        # Brightness
+        ([], "XF86MonBrightnessUp", lazy.spawn("brightnessctl set +10%")),
+        ([], "XF86MonBrightnessDown", lazy.spawn("brightnessctl set 10%-")),
+    ]
+]
