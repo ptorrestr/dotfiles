2159b0b7bc77cfbd055e6467ea47e8183b51f97e
Pablo Torres <pablo.torres.t@gmail.com>
2023-08-07 12:32:10 -0400

    improve topbar qtile

--- qtile/settings/widgets.py
+++ qtile/settings/widgets.py
@@ -1,63 +1,105 @@
+from typing import List
 from libqtile import widget
 from .theme import colors
 
 # Get the icons at https://www.nerdfonts.com/cheat-sheet (you need a Nerd Font)
 
-def base(fg='text', bg='dark'): 
-    return {
-        'foreground': colors[fg],
-        'background': colors[bg]
-    }
+
+def base(fg="text", bg="dark"):
+    return {"foreground": colors[fg], "background": colors[bg]}
 
 
 def separator():
     return widget.Sep(**base(), linewidth=0, padding=5)
 
 
-def icon(fg='text', bg='dark', fontsize=16, text="?"):
-    return widget.TextBox(
-        **base(fg, bg),
-        fontsize=fontsize,
-        text=text,
-        padding=3
-    )
+def icon(fg="text", bg="dark", fontsize=16, text="?"):
+    return widget.TextBox(**base(fg, bg), fontsize=fontsize, text=text, padding=3)
 
 
 def powerline(fg="light", bg="dark"):
     return widget.TextBox(
-        **base(fg, bg),
-        text="", # Icon: nf-oct-triangle_left
-        fontsize=37,
-        padding=-2
+        **base(fg, bg), text="", fontsize=37, padding=-3  # Icon: nf-oct-triangle_left
     )
 
 
-def workspaces(): 
+def pacman_updates(bg="color5") -> List:
+    return [
+        powerline(bg, "dark"),
+        icon(bg=bg, text="󰚰"),  # Icon: nf-fa-download
+        widget.CheckUpdates(
+            background=colors[bg],
+            colour_have_updates=colors["text"],
+            colour_no_updates=colors["text"],
+            no_update_string="0",
+            display_format="{updates}",
+            update_interval=1800,
+            custom_command="checkupdates",  # needs package pacman-contrib
+        ),
+        widget.TextBox(text="Updates", padding=3, **base(bg=bg)),
+    ]
+
+
+def network(bg="color4", prev_bg="color5") -> List:
+    return [
+        powerline(bg, prev_bg),
+        icon(bg=bg, text=" "),  # Icon: nf-fa-feed
+        widget.Net(**base(bg=bg)),
+    ]
+
+
+def layout(bg="color3", prev_bg="color4") -> List:
+    return [
+        powerline(bg, prev_bg),
+        widget.CurrentLayoutIcon(**base(bg=bg), scale=0.65),
+        widget.CurrentLayout(**base(bg=bg), padding=5),
+    ]
+
+
+def clock(bg="color2", prev_bg="color3") -> List:
+    return [
+        powerline(bg, prev_bg),
+        icon(bg=bg, fontsize=17, text=" "),  # Icon: nf-mdi-calendar_clock
+        widget.Clock(**base(bg=bg), format="%d/%m/%Y - %H:%M "),
+    ]
+
+
+def systray(bg="color4", prev_bg="color2") -> List:
+    return [
+        powerline(bg, prev_bg),
+        widget.Systray(padding=10, **base(bg=bg)),
+        widget.KeyboardLayout(
+            configured_keyboards=["latam", "us"], padding=10, **base(bg=bg)
+        ),
+    ]
+
+
+def workspaces():
     return [
         separator(),
         widget.GroupBox(
-            **base(fg='light'),
-            font='UbuntuMono Nerd Font',
+            **base(fg="light"),
+            font="UbuntuMono Nerd Font",
             fontsize=19,
             margin_y=3,
             margin_x=0,
             padding_y=8,
             padding_x=5,
             borderwidth=1,
-            active=colors['active'],
-            inactive=colors['inactive'],
+            active=colors["active"],
+            inactive=colors["inactive"],
             rounded=False,
-            highlight_method='block',
-            urgent_alert_method='block',
-            urgent_border=colors['urgent'],
-            this_current_screen_border=colors['focus'],
-            this_screen_border=colors['grey'],
-            other_current_screen_border=colors['dark'],
-            other_screen_border=colors['dark'],
+            highlight_method="block",
+            urgent_alert_method="block",
+            urgent_border=colors["urgent"],
+            this_current_screen_border=colors["focus"],
+            this_screen_border=colors["grey"],
+            other_current_screen_border=colors["dark"],
+            other_screen_border=colors["dark"],
             disable_drag=True
         ),
         separator(),
-        widget.WindowName(**base(fg='focus'), fontsize=14, padding=5),
+        widget.WindowName(**base(fg="focus"), fontsize=14, padding=5),
         separator(),
     ]
 
@@ -65,46 +107,28 @@ def workspaces():
 primary_widgets = [
     *workspaces(),
     separator(),
-    powerline('color4', 'dark'),
-    icon(bg="color4", text=' '), # Icon: nf-fa-download
-    widget.CheckUpdates(
-        background=colors['color4'],
-        colour_have_updates=colors['text'],
-        colour_no_updates=colors['text'],
-        no_update_string='0',
-        display_format='{updates}',
-        update_interval=1800,
-        custom_command='checkupdates', # needs package pacman-contrib
-    ),
-    powerline('color3', 'color4'),
-    icon(bg="color3", text=' '),  # Icon: nf-fa-feed
-    widget.Net(**base(bg='color3')),
-    powerline('color2', 'color3'),
-    widget.CurrentLayoutIcon(**base(bg='color2'), scale=0.65),
-    widget.CurrentLayout(**base(bg='color2'), padding=5),
-    powerline('color1', 'color2'),
-    icon(bg="color1", fontsize=17, text=' '), # Icon: nf-mdi-calendar_clock
-    widget.Clock(**base(bg='color1'), format='%d/%m/%Y - %H:%M '),
-    #widget.Battery(**base(bg='color1'), update_interval=1),
-    powerline('dark', 'color1'),
-    widget.Systray(background=colors['dark'], padding=5),
-    widget.KeyboardLayout(configured_keyboards=['latam', 'us']),
+    *pacman_updates(),
+    *network(),
+    *layout(),
+    *clock(),
+    # widget.Battery(**base(bg='color1'), update_interval=1),
+    *systray(),
 ]
 
 secondary_widgets = [
     *workspaces(),
     separator(),
-    powerline('color1', 'dark'),
-    widget.CurrentLayoutIcon(**base(bg='color1'), scale=0.65),
-    widget.CurrentLayout(**base(bg='color1'), padding=5),
-    powerline('color2', 'color1'),
-    widget.Clock(**base(bg='color2'), format='%d/%m/%Y - %H:%M '),
-    powerline('dark', 'color2'),
+    powerline("color1", "dark"),
+    widget.CurrentLayoutIcon(**base(bg="color1"), scale=0.65),
+    widget.CurrentLayout(**base(bg="color1"), padding=5),
+    powerline("color2", "color1"),
+    widget.Clock(**base(bg="color2"), format="%d/%m/%Y - %H:%M "),
+    powerline("dark", "color2"),
 ]
 
 widget_defaults = {
-    'font': 'UbuntuMono Nerd Font Bold',
-    'fontsize': 14,
-    'padding': 1,
+    "font": "UbuntuMono Nerd Font Bold",
+    "fontsize": 14,
+    "padding": 1,
 }
 extension_defaults = widget_defaults.copy()
